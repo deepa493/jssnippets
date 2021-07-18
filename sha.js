07f54414f1306532e6142d9cd12a35a092809af2
deepa493 <deepabalanece@gmail.com>
2021-07-18 18:34:16 +0530

    base64

--- sha.js
+++ sha.js
@@ -1,3 +1,7 @@
 var crypto= require('crypto');
+
 var sha=crypto.createHash('sha256').update('deepa').digest('hex');
 console.log(sha);
+
+var sha1=crypto.createHash('sha256').update('deepa').digest('base64');
+console.log(sha1);
